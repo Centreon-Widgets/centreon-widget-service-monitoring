37029069024f9bb2ebd09d2fdfb11a1c6ba9cd4d
Guillaume28 <gbaldo@centreon.com>
2018-04-17 11:46:14 +0200

    fix(variable) : Remove useless variable

--- service-monitoring/src/toolbar.php
+++ service-monitoring/src/toolbar.php
@@ -55,7 +55,7 @@ $template = initSmartyTplForPopup($path, $template, "./", $centreon_path);
 
 $centreon = $_SESSION['centreon'];
 $widgetId = $_POST['widgetId'];
-$pearDB = $db = $dependencyInjector['configuration_db'];
+$db = $dependencyInjector['configuration_db'];
 $widgetObj = new CentreonWidget($centreon, $db);
 $preferences = $widgetObj->getWidgetPreferences($widgetId);
 
